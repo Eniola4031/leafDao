3733b149490e350a31eba11fa922adc06593b35f
eniola4031 <agboola.eniola4031@gmail.com>
2022-03-30 17:12:19 +0100

    added roles

--- contracts/leafdo.sol
+++ contracts/leafdo.sol
@@ -12,63 +12,107 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract leafDao is Ownable {
 
     uint256 mintPrice = 0.06 ether;
-    ERC721 public lazyLionToken;
-    bool isLazyLionOwner = false;
-    bool public enableWhitelist = false;
-    bool public disableWhitelist = false;
+    string public constant whitelistRole ="whitelist";
+    string public constant adminRole = "admin";
 
 
-    constructor(address lionToken) {
-        lazyLionToken = ERC721(lionToken) ;
-
-    }
-        mapping(address => bool) holderToIsWhitelist;
-
-    // function mintLion(uint256 numberOfTokens) public payable {
-    //     require(saleIsActive, "Sale must be active to mint Lion");
-    //     require(
-    //         numberOfTokens > 0 && numberOfTokens <= maxLionPurchase,
-    //         "Can only mint 3 tokens per wallet"
-    //     );
-    //     require(
-    //         totalSupply().add(numberOfTokens) <= MAX_LIONS,
-    //         "Purchase would exceed max supply of Lions"
-    //     );
-    //     require(
-    //         msg.value >= lionPrice(numberOfTokens++)),
-    //         "Ether value sent is not correct"
-    //     );
-    // }
+                //checks if address if whitelisted
+             mapping(address => bool) public whitelist;
+             mapping(address => bool) public admin;
 
     
-    // function addToWhitelist(address user) public onlyOwner{
-    //     //require(msg.sender == user);
-    //     msg.sender = user;
-
-    // }
-
-    // function isWhitelist(address whitelisted) public returns(bool){
-    //     holderToIsWhitelist[whitelisted];
-    //     return true;
-    // }
-
-    function whitelistState() public onlyOwner{
-        enableWhitelist =!enableWhitelist;
-
-    }
-    
-
-    function mint(uint noOfTokenToMint) public view  {
-        uint lazyEditionHeadphones = 120;
-        require(isLazyLionOwner, "You must be a lazy lion owner");
-        require(noOfTokenToMint > 0 && noOfTokenToMint <= lazyEditionHeadphones, "Can only mint 1 tokens per wallet");
-        require(holderToIsWhitelist[msg.sender], "only whitelisted address can mint");
-
-
-    }
-
-    
-
+/**
+   * @dev Throws if called by any account that's not whitelisted.
+   */
+         modifier onlyIfWhitelisted() {
+    require(whitelist[msg.sender]);
+    _;
+  }
+   modifier onlyAdmin(){
+    require(admin[msg.sender]);     
+      _;
+   }
    
-   
+
+    
+  
+      event WhitelistedAddressAdded(address addr);
+      //event WhitelistedAddressRemoved(address addr);
+
+
+  /**
+   * @dev add an address to the whitelist
+   * @param addr address
+   * @notice true if the address was added to the whitelist, false if the address was already in the whitelist 
+   */
+  function addAddressToWhitelist(address addr) onlyOwner public returns(bool success) {
+    //   require(whitelist[addr] == true);
+    //   require(WhitelistedAddressAdded(addr) == true);      
+    if (!whitelist[addr]) {
+      whitelist[addr] = true;
+
+        emit WhitelistedAddressAdded(addr);
+      success = true; 
+    }
+
+  }
+  function addAddressAsAdmin(address addr) onlyOwner external returns(bool success) {
+    //    require(!admin[addr], "already an admin");
+    //    admin[addr];
+    if (!admin[addr]) {
+      admin[addr] = true;
+      success = true; 
+    }
+  }
+
+  /**
+   * @dev add addresses to the whitelist
+   * @param addrs addresses
+   * @notice true if at least one address was added to the whitelist, 
+   * false if all addresses were already in the whitelist  
+   */
+  function addAddressesToWhitelist(address[] memory addrs) onlyOwner public returns(bool success) {
+    for (uint256 i = 0; i < addrs.length; i++) {
+      if (addAddressToWhitelist(addrs[i])) {
+        success = true;
+      }
+    }
+  }
+
+
+
+    
+   function increaseMintAmt(uint tokenAmt) onlyAdmin public {
+       require(admin[msg.sender]);
+
+
+   }
+
+  //   /**
+//    * @dev remove an address from the whitelist
+//    * @param addr address
+//    * @return true if the address was removed from the whitelist, 
+//    * false if the address wasn't in the whitelist in the first place 
+//    */
+//   function removeAddressFromWhitelist(address addr) onlyOwner public returns(bool success) {
+//     if (whitelist[addr]) {
+//       whitelist[addr] = false;
+//       WhitelistedAddressRemoved(addr);
+//       success = true;
+//     }
+//   }
+
+//   /**
+//    * @dev remove addresses from the whitelist
+//    * @param addrs addresses
+//    * @return true if at least one address was removed from the whitelist, 
+//    * false if all addresses weren't in the whitelist in the first place
+//    */
+//   function removeAddressesFromWhitelist(address[] addrs) onlyOwner public returns(bool success) {
+//     for (uint256 i = 0; i < addrs.length; i++) {
+//       if (removeAddressFromWhitelist(addrs[i])) {
+//         success = true;
+//       }
+//     }
+//   } 
 }
