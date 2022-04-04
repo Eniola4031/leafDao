5739b45432c44a9eb045633305ad86fca7b1b19c
eniola4031 <agboola.eniola4031@gmail.com>
2022-04-04 12:19:45 +0100

    mint

--- contracts/leafdo.sol
+++ contracts/leafdo.sol
@@ -4,19 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+ interface lazyLionI{
+    function balanceOf(address owner) external view returns (uint256 balance);
+ }
 
 /*
 * only those with lazylion token can mint
 * 
 */
-contract leafDao is Ownable {
+contract leafDao is ERC721, Ownable {
+ using Counters for Counters.Counter;
+
+ Counters.Counter private _tokenIds;
 
     uint256 mintPrice = 0.06 ether;
     string public constant whitelistRole ="whitelist";
     string public constant adminRole = "admin";
-    bool private enableWhitelist = false;
-    bool private disableWhitelist = false;
+    bool public enableWhitelist = false;
     uint mintAmt;
+    uint maxMintKingAmount = 3;
+    uint maxMintLazyAmount = 1;
+     uint lazyMaxSupply = 120;
+     uint kingMaxSupply = 380;
+
+    lazyLionI _lazyLion = lazyLionI(0x8943C7bAC1914C9A7ABa750Bf2B6B09Fd21037E0);
 
 
 
@@ -25,7 +38,10 @@ contract leafDao is Ownable {
              mapping(address => bool) public admin;
             mapping (address => uint) public balances;
 
+    constructor()ERC721("leafDAO","LFD"){
+     // _mint(msg.sender,500);
 
+}
     
 /**
    * @dev Throws if called by any account that's not eded.
@@ -52,9 +68,8 @@ contract leafDao is Ownable {
    * @param addr address
    * @notice true if the address was added to the whitelist, false if the address was already in the whitelist 
    */
-  function addToWhitelist(address addr) onlyOwner public returns(bool success) {
-    //   require(whitelist[addr] == true);
-    //   require(WhitelistedAddressAdded(addr) == true);      
+  function addToWhitelist(address addr) onlyAdmin public returns(bool success) {
+    //require(_lazyLion.balanceOf(addr) > 0, "not a lazy lion owner");     
     if (!whitelisted[addr]) {
       whitelisted[addr] = true;
 
@@ -64,7 +79,7 @@ contract leafDao is Ownable {
 
   }
   function addAsAdmin(address addr) onlyOwner external returns(bool success) {
-    //    require(!admin[addr], "already an admin");
+    // require(!admin[addr], "already an admin");
     //    admin[addr];
     if (!admin[addr]) {
       admin[addr] = true;
@@ -79,6 +94,7 @@ contract leafDao is Ownable {
    * false if all addresses were already in the whitelist  
    */
   function addAddressesToWhitelist(address[] memory addrs) onlyOwner public returns(bool success) {
+        //require(_lazyLion.balanceOf(addrs) > 0, "not a lazy lion owner");     
     for (uint256 i = 0; i < addrs.length; i++) {
       if (addToWhitelist(addrs[i])) {
         success = true;
@@ -90,26 +106,44 @@ contract leafDao is Ownable {
         mintAmt = newMint;
     }
 
-   function mintLazyEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public {
+   function mintLazyEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public payable{
+         require(_lazyLion.balanceOf(addr) > 0, "not a lazy lion owner");     
           mintAmt = tokenamt;
          require(tokenamt > 0, "need to mint at least 1 NFT");
        require(tokenamt == 1 || tokenamt == mintAmt,"You have exceeded the max token limit per wallet");
        require(whitelisted[addr],"only whitelisted addresses can call this function");
-        balances[msg.sender] += tokenamt;
+        require(tokenamt == maxMintLazyAmount);
+        require( totalSupply() + tokenamt <= lazyMaxSupply); 
+        if ( totalSupply() >= 120) {
+                require(msg.value >= mintAmt * tokenamt);
+            } 
+        if (_tokenIds.current()==0){
+            _tokenIds.increment();
+       }
+        
+        for (uint256 i = 1; i <= tokenamt; i++) {
+            uint256 newTokenId = _tokenIds.current();
+            _safeMint(addr,newTokenId);
+            _tokenIds.increment();
+        }
+        }
+      //  balances[msg.sender] += tokenamt;
 
-   }
+   
+
+
               //error InsufficientBalance(uint requested, uint available);
-      function mintKingEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public payable{
-       uint mintKingEditionAmt= 3;
-             require(msg.value >= mintPrice);
-       require(tokenamt == mintKingEditionAmt || tokenamt == mintAmt ,"You can only mint 3 token per wallet");
-         require(tokenamt > 0, "need to mint at least 1 NFT");
-       require(tokenamt == 3 || tokenamt == mintAmt,"You have exceeded the max token limit per wallet");
-       require(whitelisted[msg.sender],"only whitelisted addresses can call this function");
-        balances[msg.sender] += tokenamt;
+  //     function mintKingEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public payable{
+  //      uint mintKingEditionAmt= 3;
+  //            require(msg.value >= mintPrice);
+  //      require(tokenamt == mintKingEditionAmt || tokenamt == mintAmt ,"You can only mint 3 token per wallet");
+  //        require(tokenamt > 0, "need to mint at least 1 NFT");
+  //      require(tokenamt == 3 || tokenamt == mintAmt,"You have exceeded the max token limit per wallet");
+  //      require(whitelisted[msg.sender],"only whitelisted addresses can call this function");
+  //       balances[msg.sender] += tokenamt;
+        
 
-
-   }
+  //  }
 
    function enableWhitelistState() public onlyAdmin{
        enableWhitelist = !enableWhitelist;
