7b47f900a064b2c9ebd646885880452a5e076470
eniola4031 <agboola.eniola4031@gmail.com>
2022-04-04 08:03:32 +0100

    abi

--- contracts/leafdo.sol
+++ contracts/leafdo.sol
@@ -14,9 +14,9 @@ contract leafDao is Ownable {
     uint256 mintPrice = 0.06 ether;
     string public constant whitelistRole ="whitelist";
     string public constant adminRole = "admin";
-    bool public enableWhitelist = false;
-    bool public disableWhitelist = false;
-    uint mintLazyEditionAmt = 1;
+    bool private enableWhitelist = false;
+    bool private disableWhitelist = false;
+    uint mintAmt;
 
 
 
@@ -35,7 +35,6 @@ contract leafDao is Ownable {
     _;
   }
    modifier onlyAdmin(){
-         //  require(msg.sender == owner);
 
     require(admin[msg.sender]);  
       _;
@@ -87,64 +86,34 @@ contract leafDao is Ownable {
     }
   }
 
-
-
-    
-   function increaseMintAmt() onlyAdmin public {
-       require(admin[msg.sender],"the method can only be called by the admin");
-         mintLazyEditionAmt++;
-
-   }
+       function changeMintAmt(uint256 newMint) public onlyAdmin {
+        mintAmt = newMint;
+    }
 
    function mintLazyEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public {
-       //mintLazyEditionAmt= tokenamt;
-       require(tokenamt == mintLazyEditionAmt,"You can only mint 1 token per wallet");
-       require(whitelisted[addr],"you are not whitelisted");
-       balances[msg.sender] += tokenamt;
+          mintAmt = tokenamt;
+         require(tokenamt > 0, "need to mint at least 1 NFT");
+       require(tokenamt == 1 || tokenamt == mintAmt,"You have exceeded the max token limit per wallet");
+       require(whitelisted[addr],"only whitelisted addresses can call this function");
+        balances[msg.sender] += tokenamt;
 
    }
               //error InsufficientBalance(uint requested, uint available);
-
       function mintKingEdition(address addr, uint tokenamt) onlyIfWhitelisted(addr) public payable{
        uint mintKingEditionAmt= 3;
-       require(tokenamt == mintKingEditionAmt,"You can only mint 3 token per wallet");
-       require(whitelisted[addr],"you are not whitelisted");
-       balances[msg.sender] += tokenamt;
+             require(msg.value >= mintPrice);
+       require(tokenamt == mintKingEditionAmt || tokenamt == mintAmt ,"You can only mint 3 token per wallet");
+         require(tokenamt > 0, "need to mint at least 1 NFT");
+       require(tokenamt == 3 || tokenamt == mintAmt,"You have exceeded the max token limit per wallet");
+       require(whitelisted[msg.sender],"only whitelisted addresses can call this function");
+        balances[msg.sender] += tokenamt;
 
 
    }
 
-
-   function whitelistState(bool _state) public onlyAdmin{
-       enableWhitelist = _state;
+   function enableWhitelistState() public onlyAdmin{
+       enableWhitelist = !enableWhitelist;
 
    }
 
-  //   /**
-//    * @dev remove an address from the whitelist
-//    * @param addr address
-//    * @return true if the address was removed from the whitelist, 
-//    * false if the address wasn't in the whitelist in the first place 
-//    */
-//   function removeAddressFromWhitelist(address addr) onlyOwner public returns(bool success) {
-//     if (whitelist[addr]) {
-//       whitelist[addr] = false;
-//       WhitelistedAddressRemoved(addr);
-//       success = true;
-//     }
-//   }
-
-//   /**
-//    * @dev remove addresses from the whitelist
-//    * @param addrs addresses
-//    * @return true if at least one address was removed from the whitelist, 
-//    * false if all addresses weren't in the whitelist in the first place
-//    */
-//   function removeAddressesFromWhitelist(address[] addrs) onlyOwner public returns(bool success) {
-//     for (uint256 i = 0; i < addrs.length; i++) {
-//       if (removeAddressFromWhitelist(addrs[i])) {
-//         success = true;
-//       }
-//     }
-//   } 
 }
